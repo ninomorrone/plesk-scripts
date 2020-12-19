8c931754cd48d4d9d21bc4b1a729cf03ceface83
Nino Morrone <emorrone@icnet.it>
2020-12-19 18:56:00 +0100

    Cartella BAK in root

--- do_full_bak.sh
+++ do_full_bak.sh
@@ -7,13 +7,14 @@ ODIR="bak-db" #folder temporanea per mysql
 OWWW="domini"
 SITI="/var/www/vhosts/" #path base plesk
 DCUR=$PWD # folder corrente
+LINE="----------------------------------------"
 
 #:<<REM
-clear && echo " ---------- [ MySQL backup ] ----------"
-rm -rf $ODIR $OWWW
+clear
+echo "[ MySQL backup ]"
+echo $LINE
 
-# rm -f $ODIR.tar.gz
-# rm -rf *.tar.gz
+rm -rf $ODIR $OWWW bak
 
 mysql -u $USER -p$PASS -e "show databases" \
     | grep -Ev 'Database|information_schema|performance_schema|mysql|psa|phpmyadmin|horde|apsc|roundcubemail' \
@@ -23,18 +24,18 @@ do
   mkdir -p $ODIR
   mysqldump -u $USER -p$PASS $dbname > ./$ODIR/$dbname.sql
 done
-echo " "
-echo " ---------- [ Comprimo il backup ] ----------"
 tar czf $ODIR.tar.gz $ODIR
 rm -rf $ODIR
-echo " "
-echo "***********************************"
-echo "***     Backup DB ok            ***"
-echo "***********************************"
+echo $LINE
+echo "[ MySQL backup ] OK"
+echo $LINE
 echo " "
 #REM
 
 mkdir -p $OWWW
+
+echo "[ BACKUPS SITI ]"
+echo $LINE
 
 mysql -u $USER -p$PASS -se "USE psa;select name,h.www_root as hfolder from domains as d inner join hosting as h on d.id = dom_id;" \
     | while read -r dominio hfolder;
@@ -42,7 +43,7 @@ do
 
         sitepath=${hfolder#"$SITI$dominio/"} # elimina l'url base dela percorso completo restituisce httpdocs o la cartella subdomain
 
-        echo " ---------- [Backup  $dominio] ----------"
+        echo " ... $dominio"
 
         cd $SITI # posizionamento nella cartella base di plesk
 
@@ -63,6 +64,11 @@ cd $DCUR # ritoro alla cartella iniziale
 tar czf $OWWW.tar.gz $OWWW
 rm -rf $OWWW
 
-echo "***********************************"
-echo "***     Backup DOMINI OK        ***"
-echo "***********************************"
+mkdir -p bak
+mv *.tar.gz bak
+
+
+echo $LINE
+echo "[ SITI ] ok"
+echo $LINE
+echo " "
